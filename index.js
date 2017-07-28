30b91d927c34ad5705ceb2c08d4094fa2000c6e0
chie_4 <w310930920@gmail.com>
2017-07-28 22:34:55 +0800

    sldkfj

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var pngFileStream = require('png-file-stream');
 var fs = require('fs');
 const path = require('path');
 
-// pngFileStream('node_modules/gifencoder/test/**/frame?.png').pipe(encoder.createWriteStream({ repeat: -1, delay: 500, quality: 10 }))
+// pngFileStream('node_modules/gifencoder/test/**/frame?.png')
+//   .pipe(encoder.createWriteStream({ repeat: -1, delay: 500, quality: 10 }))
 //   .pipe(fs.createWriteStream('myanimated.gif'));
 
 const b = pngFileStream('node_modules/gifencoder/test/**/frame?.png');
-const t = path.resolve(process.cwd(),'node_modules/gifencoder/test/**/frame?.png')
-// console.log(b);
+const t = path.resolve(process.cwd(), 'node_modules/gifencoder/test/**/frame?.png');
